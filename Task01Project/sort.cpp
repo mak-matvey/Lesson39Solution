188ad09129d3e5e23a6932615770e2f3ae9d84e1
Matvey Makushenko <matvey.myown@gmail.com>
2025-02-13 18:05:17 +0300

    Upgrade the function for best situation

--- Task01Project/sort.cpp
+++ Task01Project/sort.cpp
@@ -4,17 +4,23 @@
 void bubble_sort(int* array, int size)
 {
 	int count = 0;
+	bool flag = true;
 	for (int j = 0; j < size - 1; j++)
 	{
-		for (int i = 0; i < size - 1 ; i++)
+		for (int i = 0; i < size - 1 - j; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				int t = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = t;
-				count++;
+				flag = false;
 			}
+			count++;
+		}
+		if (flag)
+		{
+			break;
 		}
 	}
 
